ba1028cb80e48b60967882cf34aed68a16ff9e1a
agordo25 <34726753+agordo25@users.noreply.github.com>
2018-02-01 13:34:18 -0500

    Add files via upload

---
+++ Milespergallon 1:30.py
@@ -0,0 +1,7 @@
+while True:
+    print("This program caclulates miles per gallon")
+    miles_driven=float(input("Enter miles driven"))
+    gallons_used=float(input("Enter gallons used:"))
+    milesper_gallon=miles_driven/gallons_used
+    print("Your miles per gallon is")
+    print(milesper_gallon)
